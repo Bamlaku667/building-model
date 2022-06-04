5647716ac9633476c17dc3c9229b5b3d0f493019
Bamlaku <bamlakhiruya@gmail.com>
2022-06-04 06:28:20 +0300

    initializing a checkers screen

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame as pg
+from OpenGL.GL import *
+
+
+class Checkers:
+    def __init__(self):
+        self.width = 800  # board width
+        self.height = 800  # board height
+        self.cols = 8
+        self.rows = 8
+        self.cell_size = self.width // self.cols
+        self.clock = pg.time.Clock()
+        pg.init()
+        self.screen = pg.display.set_mode((self.width, self.height), pg.OPENGL | pg.DOUBLEBUF)
+        glClearColor(0.1, 0.2, 0.2, 1)  # initialize openGl
+        self.main()
+
+    def main(self):
+        running = True
+        while running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+            glClear(GL_COLOR_BUFFER_BIT)
+            pg.display.flip()
+
+            self.clock.tick(60)           # for constant frame rating
+
+
+
+        pg.quit()
+
+
+if __name__ == "__main__":
+    checkers = Checkers()
+
+
