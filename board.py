40dab7ba10e70b67dcd28cd194bc06c4f2837ffd
Bamlaku Hiruy <bamlakhiruya@gmail.com>
2022-06-13 14:48:07 +0300

    adding board colors

---
+++ board.py
@@ -0,0 +1,12 @@
+
+
+class Board:
+    def __init__(self):
+        self.board = [[white, black, white, black, white, black, white, black ],
+                      [black, white, black, white, black, white, black, white ],
+                      [white, black, white, black, white, black, white, black ],
+                      [black, white, black, white, black, white, black, white],
+                      [white, black, white, black, white, black, white, black ],
+                      [black, white, black, white, black, white, black, white],
+                      [white, black, white, black, white, black, white, black],
+                      [black, white, black, white, black, white, black, white]]
