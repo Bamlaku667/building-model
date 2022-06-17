2d5577d9fc457e27801ab4fcc394b8a5047d5078
Bamlaku Hiruy <bamlakhiruya@gmail.com>
2022-06-17 05:18:50 +0300

    drawing the board and adding checker pieces

--- main.py
+++ main.py
@@ -1,43 +1,67 @@
-import random
-
 import pygame as pg
-from OpenGL.GL import *
+from game import *
+width = 700
+height = 700
+rows = 8
+cols = 8
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+brown = (120,42,42)
+squareSize = width // rows
+
+image1 = pg.transform.scale(pg.image.load('images/checkers.png'), (60, 60))
+image2 = pg.transform.scale(pg.image.load('images/image2.jpg'), (60, 60))
+
+
+def drawAll(screen, board):
+    drawBoard(screen)
+    drawPiece(screen, board)
+
+def drawBoard(window):
+    window.fill(brown)
+    board = Game().board
+    for row in range(rows):
+        for col in range(cols):
+            if board[row][col] == "w":
+                pg.draw.rect(window, white, (col * squareSize, row * squareSize, squareSize, squareSize))
 
 
 
 
-class Checkers:
-    def __init__(self):
-        self.width = 800  # board width
-        self.height = 800  # board height
-        self.cols = 8
-        self.rows = 8
-        self.cell_size = self.width // self.cols
-        self.clock = pg.time.Clock()
-        pg.init()
-        self.screen = pg.display.set_mode((self.width, self.height), pg.OPENGL | pg.DOUBLEBUF)
-        glClearColor(0.1, 0.2, 0.2, 1)  # initialize openGl
-        self.main()
 
-    def main(self):
-        running = True
-        while running:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    running = False
-            glClear(GL_COLOR_BUFFER_BIT)
-            pg.display.flip()
 
-            self.clock.tick(60)           # for constant frame rating
+def drawPiece(screen, board):
+    for row in range(rows):
+        for col in range(cols):
+            if board[row][col] == "w" :
+                if row < 3:
+                    screen.blit(image1, (col * squareSize, row * squareSize, squareSize // 2, squareSize // 2))
+                if row > 4:
+                    screen.blit(image2, (col * squareSize, row * squareSize,  squareSize, squareSize))
+
+
+def main():
+    pg.init()
+    window = pg.display.set_mode((width, height))
+    clock = pg.time.Clock()
+    window.fill(blue)
+    running = True
+    board = Game().board
 
 
 
-        pg.quit()
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
 
-players = ['x', 'o']
-turn = random.randrange(2)
-print(turn)
-print("%s's turn" % players[turn ])
+        clock.tick(60)
+        drawAll(window, board)
+        pg.display.update()
+
+    pg.quit()
 
 
-
+main()
