f99e655297450a826fe208072904a0908963ac16
Bamlaku Hiruy <bamlakhiruya@gmail.com>
2022-06-19 00:41:46 +0300

    editing the object loader file to include mtl loader

--- loader.py
+++ loader.py
@@ -1,238 +1,102 @@
-import numpy as np
-import pygame as pg
+import pygame
+
 from OpenGL.GL import *
-from OpenGL.GL.shaders import compileProgram, compileShader
-import pyrr
+
+def MTL(filename='checker_board3.mtl'):
+    contents = {}
+    mtl = None
+    for line in open(filename, "r"):
+        if line.startswith('#'): continue
+        values = line.split()
+        if not values: continue
+        if values[0] == 'newmtl':
+            mtl = contents[values[1]] = {}
+        elif mtl is None:
+            raise ValueError("mtl file doesn't start with newmtl stmt")
+        elif values[0] == 'map_Kd':
+            # load the texture referred to by this declaration
+            mtl[values[0]] = values[1]
+            surf = pygame.image.load(mtl['map_Kd'])
+            image = pygame.image.tostring(surf, 'RGBA', 1)
+            ix, iy = surf.get_rect().size
+            texid = mtl['texture_Kd'] = glGenTextures(1)
+            glBindTexture(GL_TEXTURE_2D, texid)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+                GL_LINEAR)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA,
+                GL_UNSIGNED_BYTE, image)
+        else:
+            mtl[values[0]] = list(map(float, values[1:]))
+    return contents
 
 
-'''
- This file used to load the obj file 
-'''
+class OBJ:
+    def __init__(self, filename, swapyz=False):
+        """Loads a Wavefront OBJ file. """
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+        material = None
+        for line in open(filename, "r"):
+            if line.startswith('#'): continue
+            values = line.split()
+            if not values: continue
+            if values[0] == 'v':
+                v = list(map(float, values[1:4]))
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.vertices.append(v)
+            elif values[0] == 'vn':
+                v = list(map(float, values[1:4]))
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.normals.append(v)
+            elif values[0] == 'vt':
+                self.texcoords.append(list(map(float, values[1:3])))
+            elif values[0] in ('usemtl', 'usemat'):
+                material = values[1]
+            elif values[0] == 'mtllib':
+                self.mtl = MTL(values[1])
+            elif values[0] == 'f':
+                face = []
+                texcoords = []
+                norms = []
+                for v in values[1:]:
+                    w = v.split('/')
+                    face.append(int(w[0]))
+                    if len(w) >= 2 and len(w[1]) > 0:
+                        texcoords.append(int(w[1]))
+                    else:
+                        texcoords.append(0)
+                    if len(w) >= 3 and len(w[2]) > 0:
+                        norms.append(int(w[2]))
+                    else:
+                        norms.append(0)
+                self.faces.append((face, norms, texcoords, material))
+        self.gl_list = glGenLists(1)
+        glNewList(self.gl_list, GL_COMPILE)
+        glEnable(GL_TEXTURE_2D)
+        glFrontFace(GL_CCW)
+        for face in self.faces:
+            vertices, normals, texture_coords, material = face
+            mtl = self.mtl[material]
+            if 'texture_Kd' in mtl:
+                # use diffuse texmap
+                glBindTexture(GL_TEXTURE_2D, mtl['texture_Kd'])
+            else:
+                # just use diffuse colour
+                glColor(*mtl['Kd'])
 
-class Mesh:
-    def __init__(self, filePath):
-        self.vertices = self.loadMesh(filePath)
-        self.vertex_count = len(self.vertices)
-        self.vertices = np.array(self.vertices, dtype=np.float32)
-
-        self.vao = glGenVertexArrays(1)
-        glBindVertexArray(self.vao)
-        self.vbo = glGenBuffers(1)
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
-        # position
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
-        # texture
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
-
-    def loadMesh(self, filename):
-
-        # raw, unassembled data
-        v = []
-        vt = []
-        vn = []
-
-        # final, assembled and packed result
-        vertices = []
-
-        # open the obj file and read the data
-        with open(filename, 'r') as f:
-            line = f.readline()
-            while line:
-                firstSpace = line.find(" ")
-                flag = line[0:firstSpace]
-                if flag == "v":
-                    # vertex
-                    line = line.replace("v ", "")
-                    line = line.split(" ")
-                    l = [float(x) for x in line]
-                    v.append(l)
-                elif flag == "vt":
-                    # texture coordinate
-                    line = line.replace("vt ", "")
-                    line = line.split(" ")
-                    l = [float(x) for x in line]
-                    vt.append(l)
-                elif flag == "vn":
-                    # normal
-                    line = line.replace("vn ", "")
-                    line = line.split(" ")
-                    l = [float(x) for x in line]
-                    vn.append(l)
-                elif flag == "f":
-                    # face, three or more vertices in v/vt/vn form
-                    line = line.replace("f ", "")
-                    line = line.replace("\n", "")
-                    # get the individual vertices for each line
-                    line = line.split(" ")
-                    faceVertices = []
-                    faceTextures = []
-                    faceNormals = []
-                    for vertex in line:
-                        # break out into [v,vt,vn],
-                        # correct for 0 based indexing.
-                        l = vertex.split("/")
-                        position = int(l[0]) - 1
-                        faceVertices.append(v[position])
-                        texture = int(l[1]) - 1
-                        faceTextures.append(vt[texture])
-                        normal = int(l[2]) - 1
-                        faceNormals.append(vn[normal])
-                    # obj file uses triangle fan format for each face individually.
-                    # unpack each face
-                    triangles_in_face = len(line) - 2
-
-                    vertex_order = []
-                    """
-                        eg. 0,1,2,3 unpacks to vertices: [0,1,2,0,2,3]
-                    """
-                    for i in range(triangles_in_face):
-                        vertex_order.append(0)
-                        vertex_order.append(i + 1)
-                        vertex_order.append(i + 2)
-                    for i in vertex_order:
-                        for x in faceVertices[i]:
-                            vertices.append(x)
-                        for x in faceTextures[i]:
-                            vertices.append(x)
-                        for x in faceNormals[i]:
-                            vertices.append(x)
-                line = f.readline()
-        return vertices
-
-
-class Material:
-
-    def __init__(self, filepath):
-        self.texture = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, self.texture)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        image = pg.image.load(filepath).convert()
-        image_width, image_height = image.get_rect().size
-        img_data = pg.image.tostring(image, 'RGBA')
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
-        glGenerateMipmap(GL_TEXTURE_2D)
-
-    def use(self):
-        glActiveTexture(GL_TEXTURE0)
-        glBindTexture(GL_TEXTURE_2D, self.texture)
-
-    def destroy(self):
-        glDeleteTextures(1, (self.texture,))
-
-
-class Cube:
-
-    def __init__(self, position, eulers):
-        self.position = np.array(position, dtype=np.float32)
-        self.eulers = np.array(eulers, dtype=np.float32)
-
-
-
-
-
-class App:
-    def __init__(self):
-        # initialise pygame
-        pg.init()
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
-                                    pg.GL_CONTEXT_PROFILE_CORE)
-        pg.display.set_mode((640, 480), pg.OPENGL | pg.DOUBLEBUF)
-        self.clock = pg.time.Clock()
-        # initialise opengl
-        glClearColor(0.1, 0.2, 0.2, 1)
-        self.shader = self.createShader("shaders/vertex_shader.txt", "shaders/fragment_shader.txt")
-        glUseProgram(self.shader)
-        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
-        glEnable(GL_DEPTH_TEST)
-
-        self.cube_mesh = Mesh("checker_board.obj")
-        self.wood_texture = Material("images/checkers_texture.jpg")
-
-        self.cube = Cube(
-            position=[0, 0, -3],
-            eulers=[0, 0, 0]
-        )
-
-        projection_transform = pyrr.matrix44.create_perspective_projection(
-            fovy=45, aspect=640 / 480,
-            near=0.1, far=10, dtype=np.float32
-        )
-        glUniformMatrix4fv(
-            glGetUniformLocation(self.shader, "projection"),
-            1, GL_FALSE, projection_transform
-        )
-        self.modelMatrixLocation = glGetUniformLocation(self.shader, "model")
-        self.mainLoop()
-
-    def createShader(self, vertexFilepath, fragmentFilepath):
-
-        with open(vertexFilepath, 'r') as f:
-            vertex_src = f.readlines()
-
-        with open(fragmentFilepath, 'r') as f:
-            fragment_src = f.readlines()
-
-        shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER),
-                                compileShader(fragment_src, GL_FRAGMENT_SHADER))
-
-        return shader
-
-    def mainLoop(self):
-        running = True
-        while (running):
-            # check events
-            for event in pg.event.get():
-                if (event.type == pg.QUIT):
-                    running = False
-
-            # update cube
-            self.cube.eulers[2] += 0.25
-            if self.cube.eulers[2] > 360:
-                self.cube.eulers[2] -= 360
-
-            # refresh screen
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            glUseProgram(self.shader)
-
-            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
-            """
-                pitch: rotation around x axis
-                roll:rotation around z axis
-                yaw: rotation around y axis
-            """
-            model_transform = pyrr.matrix44.multiply(
-                m1=model_transform,
-                m2=pyrr.matrix44.create_from_eulers(
-                    eulers=np.radians(self.cube.eulers), dtype=np.float32
-                )
-            )
-            model_transform = pyrr.matrix44.multiply(
-                m1=model_transform,
-                m2=pyrr.matrix44.create_from_translation(
-                    vec=np.array(self.cube.position), dtype=np.float32
-                )
-            )
-            glUniformMatrix4fv(self.modelMatrixLocation, 1, GL_FALSE, model_transform)
-            glBindVertexArray(self.cube_mesh.vao)
-            glDrawArrays(GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
-
-            pg.display.flip()
-
-            # timing
-            self.clock.tick(60)
-        self.quit()
-
-    def quit(self):
-        glDeleteProgram(self.shader)
-        pg.quit()
-
-
-myApp = App()
+            glBegin(GL_POLYGON)
+            for i in range(len(vertices)):
+                if normals[i] > 0:
+                    glNormal3fv(self.normals[normals[i] - 1])
+                if texture_coords[i] > 0:
+                    glTexCoord2fv(self.texcoords[texture_coords[i] - 1])
+                glVertex3fv(self.vertices[vertices[i] - 1])
+            glEnd()
+        glDisable(GL_TEXTURE_2D)
+        glEndList()
