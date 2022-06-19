d749fc7d29a3d0e6aeb2cc7e296028bcb503f50b
Bamlaku Hiruy <bamlakhiruya@gmail.com>
2022-06-19 09:03:52 +0300

    separating the mtl loader file

--- main.py
+++ main.py
@@ -1,67 +1,73 @@
-import pygame as pg
-from game import *
-width = 700
-height = 700
-rows = 8
-cols = 8
-black = (0, 0, 0)
-white = (255, 255, 255)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-brown = (120,42,42)
-squareSize = width // rows
+# IMPORT OBJECT LOADER
+import sys, pygame
 
-image1 = pg.transform.scale(pg.image.load('images/checkers.png'), (60, 60))
-image2 = pg.transform.scale(pg.image.load('images/image2.jpg'), (60, 60))
+from OpenGL.raw.GLU import gluPerspective
+from pygame.locals import *
+from pygame.constants import *
+from OpenGL.GL import  *
+from objloader import *
 
+pygame.init()
+viewport = (1000,800)
+hx = viewport[0]/2
+hy = viewport[1]/2
+srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
 
-def drawAll(screen, board):
-    drawBoard(screen)
-    drawPiece(screen, board)
+glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
+glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
+glEnable(GL_LIGHT0)
+glEnable(GL_LIGHTING)
+glEnable(GL_COLOR_MATERIAL)
+glEnable(GL_DEPTH_TEST)
+glShadeModel(GL_SMOOTH)
 
-def drawBoard(window):
-    window.fill(brown)
-    board = Game().board
-    for row in range(rows):
-        for col in range(cols):
-            if board[row][col] == "w":
-                pg.draw.rect(window, white, (col * squareSize, row * squareSize, squareSize, squareSize))
+obj = OBJ('checker_board3.obj')
 
+clock = pygame.time.Clock()
 
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+width, height = viewport
+gluPerspective(90.0, width/float(height), 1, 100.0)
+glEnable(GL_DEPTH_TEST)
+glMatrixMode(GL_MODELVIEW)
 
+rx, ry = (0,0)
+tx, ty = (0,0)
+zpos = 5
+rotate = move = False
+while 1:
+    clock.tick(30)
+    for e in pygame.event.get():
+        if e.type == QUIT:
+            sys.exit()
+        elif e.type == KEYDOWN and e.key == K_ESCAPE:
+            sys.exit()
+        elif e.type == MOUSEBUTTONDOWN:
+            if e.button == 4: zpos = max(1, zpos-1)
+            elif e.button == 5: zpos += 1
+            elif e.button == 1: rotate = True
+            elif e.button == 3: move = True
+        elif e.type == MOUSEBUTTONUP:
+            if e.button == 1: rotate = False
+            elif e.button == 3: move = False
+        elif e.type == MOUSEMOTION:
+            i, j = e.rel
+            if rotate:
+                rx += i
+                ry += j
+            if move:
+                tx += i
+                ty -= j
 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
 
+    # RENDER OBJECT
+    glTranslate(tx/20., ty/20., - zpos)
+    glRotate(ry, 1, 0, 0)
+    glRotate(rx, 0, 1, 0)
+    glCallList(obj.gl_list)
 
-def drawPiece(screen, board):
-    for row in range(rows):
-        for col in range(cols):
-            if board[row][col] == "w" :
-                if row < 3:
-                    screen.blit(image1, (col * squareSize, row * squareSize, squareSize // 2, squareSize // 2))
-                if row > 4:
-                    screen.blit(image2, (col * squareSize, row * squareSize,  squareSize, squareSize))
-
-
-def main():
-    pg.init()
-    window = pg.display.set_mode((width, height))
-    clock = pg.time.Clock()
-    window.fill(blue)
-    running = True
-    board = Game().board
-
-
-
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-
-        clock.tick(60)
-        drawAll(window, board)
-        pg.display.update()
-
-    pg.quit()
-
-
-main()
+    pygame.display.flip()
